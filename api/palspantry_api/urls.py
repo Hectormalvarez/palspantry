dc0e4fb3141226ef938937bb22c441c76297150d
Hector Alvarez <alvarez.hectorm@outlook.com>
2024-03-26 20:07:15 -0500

    api: added testing to Django API service

--- api/palspantry_api/urls.py
+++ api/palspantry_api/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import ping
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("v1/ping/", ping, name="ping")
 ]
